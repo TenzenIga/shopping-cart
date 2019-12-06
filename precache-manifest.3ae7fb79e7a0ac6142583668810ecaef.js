a11ecbe83c87e002b0271a855155df678f2b2008
TenzenIga <arhan-93@list.ru>
2019-12-06 06:08:36 +0300

    Updates

---
+++ precache-manifest.3ae7fb79e7a0ac6142583668810ecaef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4b94da806fb0770f5d2c5a0108ab5a",
+    "url": "/github.io/shopping-cart/index.html"
+  },
+  {
+    "revision": "4ccd1809401d0d75723c",
+    "url": "/github.io/shopping-cart/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "157c7c1c15d78c8abbb1",
+    "url": "/github.io/shopping-cart/static/js/2.6b627b34.chunk.js"
+  },
+  {
+    "revision": "4ccd1809401d0d75723c",
+    "url": "/github.io/shopping-cart/static/js/main.8d00b47f.chunk.js"
+  },
+  {
+    "revision": "f5f66123c0695cbb8729",
+    "url": "/github.io/shopping-cart/static/js/runtime-main.1f3e1061.js"
+  }
+]);
